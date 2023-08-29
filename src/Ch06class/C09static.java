006d21946e8cc37329b13f013169509c81a528ba
Kim Dohyuen <dh_bug@naver.com>
2023-08-29 10:27:45 +0900

    static 개념 설명

--- src/Ch06class/C09static.java
+++ src/Ch06class/C09static.java
@@ -1,7 +1,14 @@
 package Ch06class;
 
 public class C09static {
+    public static void main(String[] args) {
+        System.out.println("인스턴스멤버 출력");
+        MyClass09 o1 = new MyClass09();
+        o1.printName();
 
+        System.out.println("static 클래스멤버 출력");
+        MyClass09.printModel();
+    }
 }
 
 class MyClass09 {
@@ -12,11 +19,11 @@ class MyClass09 {
     //instance method
     void printName() {
         // instance member 끼리 접근 가능
-        System.out.println("name = " + name);
+        System.out.println("인스턴스 name = " + name);
 
         //instance member에서 static member 접근 가능
         //static 클래스 멤버는 하나여서 접근 대상이 명확하여 가능
-        System.out.println("model = " + model);
+        System.out.println("클래스 model = " + model);
     }
 
     static void printModel() {
