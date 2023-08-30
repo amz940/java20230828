7ccf00383e2b90288a314542712beb581e344f59
Kim Dohyuen <dh_bug@naver.com>
2023-08-30 16:53:47 +0900

    .

---
+++ src/ch08extends/book/sec07/exam14/Activity.java
@@ -0,0 +1,19 @@
+package ch08extends.book.sec07.exam14;
+
+public class Activity {
+    public static void main(String[] args) {
+        MainActivity main = new MainActivity();
+        main.onCreate();
+
+    }
+    void onCreate(){
+        System.out.println("기본적인 실행 내용");
+    }
+}
+
+class MainActivity extends Activity{
+    void onCreate(){
+        super.onCreate();
+        System.out.println("추가적인 실행 내용");
+    }
+}
