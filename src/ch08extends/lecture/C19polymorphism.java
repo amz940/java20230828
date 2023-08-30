d9c02da03180dc48a40ac8ac6561546c6ad88e84
Kim Dohyuen <dh_bug@naver.com>
2023-08-30 12:16:11 +0900

    상속 설명

---
+++ src/ch08extends/lecture/C19polymorphism.java
@@ -0,0 +1,38 @@
+package ch08extends.lecture;
+
+public class C19polymorphism {
+    public static void main(String[] args) {
+        Hero19 myHero = new Hero19();
+        myHero.weapon = new Gun();
+        myHero.attack();
+
+        myHero.weapon = new Sword();
+        myHero.attack();
+
+    }
+}
+
+class Hero19 {
+    Weapon weapon;
+
+    public void attack(){
+        weapon.shot();
+    }
+}
+
+class Weapon{
+    void shot(){
+        System.out.println("무기를 사용합니다");
+    }
+}
+
+class Gun extends Weapon{
+    void shot(){
+        System.out.println("총을 쏩니다");
+    }
+}
+class Sword extends Weapon{
+    void shot(){
+        System.out.println("검을 휘두릅니다");
+    }
+}
