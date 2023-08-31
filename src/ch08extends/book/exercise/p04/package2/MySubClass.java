69aaa09af2f64a22c534bdbd1ba2902ef1b0718f
Kim Dohyuen <dh_bug@naver.com>
2023-08-31 10:27:26 +0900

    접근 제어자 예시

---
+++ src/ch08extends/book/exercise/p04/package2/MySubClass.java
@@ -0,0 +1,18 @@
+package ch08extends.book.exercise.p04.package2;
+
+import ch08extends.book.exercise.p04.package1.MyClass;
+
+public class MySubClass extends MyClass {
+    public void method1(){
+
+    }
+    protected void method2(){
+
+    }
+    public void method3(){
+
+    }
+//    void method3(){   //부모보다 작은 접근제어자를 붙일 시 오류가 발생
+//
+//    }
+}
--- src/ch08extends/book/sec07/exam151/C.java
+++ src/ch08extends/book/sec07/exam151/C.java
@@ -3,10 +3,9 @@ package ch08extends.book.sec07.exam151;
 import ch08extends.book.sec07.exam15.A;
 
 public class C {       //A랑 다른 패키지라 사용이 안된다
-//    private A ;
 //
 //    public void method(){
-//        A  = new A();
+//        A a = new A();
 //        a.field = "value";
 //        a.method();
 //    }
