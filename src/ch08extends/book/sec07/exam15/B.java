b433f033c41d596565535a0778890c61e73cf598
Kim Dohyuen <dh_bug@naver.com>
2023-08-31 09:59:17 +0900

    접근 제어자 예시

---
+++ src/ch08extends/book/sec07/exam15/B.java
@@ -0,0 +1,9 @@
+package ch08extends.book.sec07.exam15;
+
+public class B {
+    public void method(){       // 같은 패키지 여서 가능
+        A a = new A();
+        a.field = "value";
+        a.method();
+    }
+}
---
+++ src/ch08extends/book/sec07/exam151/C.java
@@ -0,0 +1,13 @@
+package ch08extends.book.sec07.exam151;
+
+import ch08extends.book.sec07.exam15.A;
+
+public class C {       //A랑 다른 패키지라 사용이 안된다
+//    private A ;
+//
+//    public void method(){
+//        A  = new A();
+//        a.field = "value";
+//        a.method();
+//    }
+}
