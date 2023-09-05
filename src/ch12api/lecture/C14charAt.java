a064b570e263c4c7c0ea83d44674eac9268c3cda
Kim Dohyuen <dh_bug@naver.com>
2023-09-05 16:50:27 +0900

    charAt()

---
+++ src/ch12api/lecture/C14charAt.java
@@ -0,0 +1,20 @@
+package ch12api.lecture;
+
+public class C14charAt {
+    public static void main(String[] args) {
+        // charAt : 특정 인덱스의 char 리턴
+
+//        String a = "spring";
+//        char c = a.charAt(0);
+//        System.out.println(c);  // s
+//        System.out.println(a.charAt(1)); // p
+//        System.out.println(a.charAt(a.length()-1)); // g
+////        System.out.println(a.charAt(6));
+//
+//        System.out.println(a.charAt(0)+0); // ? 115?
+//        System.out.println(a.charAt(0)+"");
+
+
+        }
+    }
+
