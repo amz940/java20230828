a88659ab26974f3e81faa7d474412b880cc2e1e7
Kim Dohyuen <dh_bug@naver.com>
2023-09-05 16:47:37 +0900

    toString 사용법

---
+++ src/ch12api/lecture/C03toString.java
@@ -0,0 +1,42 @@
+package ch12api.lecture;
+
+public class C03toString {
+    public static void main(String[] args) {
+        Car car1 = new Car("tesla", 5000);
+        Car car2 = new Car("kia", 3000);
+        Car car3 = new Car("bmw", 2000);
+
+//        System.out.println(car1.getModel() + ":" + car1.getPrice());
+//        System.out.println(car2.getModel() + ":" + car2.getPrice());
+//        System.out.println(car3.getModel() + ":" + car3.getPrice());
+
+        System.out.println(car1.toString());
+        System.out.println(car1);
+    }
+}
+
+class Car{
+    private String model;
+    private int price;
+
+    public Car(String model, int price){
+        this.model = model;
+        this.price = price;
+
+    }
+
+    public String getModel(){
+        return model;
+    }
+    public int getPrice(){
+        return price;
+    }
+
+    public String toString(){
+        return this.model + ": " + this.price;
+    }
+
+
+}
+
+
