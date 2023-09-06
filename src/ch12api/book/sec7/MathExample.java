2c228e7ab92ce3a242644f73089dded156710a87
Kim Dohyuen <dh_bug@naver.com>
2023-09-06 16:37:38 +0900

    Math에서 주로 사용하는 코드

---
+++ src/ch12api/book/sec7/MathExample.java
@@ -0,0 +1,26 @@
+package ch12api.book.sec7;
+
+public class MathExample {
+    public static void main(String[] args) {
+            // 올림 값
+        double v1 = Math.ceil(5.3);
+            // 내림 값
+        double v2 = Math.floor(5.3);
+        System.out.println("v1 = " + v1);
+        System.out.println("v2 = " + v2);
+            //높은 숫자
+        long v3 = Math.max(3,7);
+            //낮은 숫자
+        long v4 = Math.min(6,5);
+        System.out.println("v3 = " + v3);
+        System.out.println("v4 = " + v4);
+
+        double value = 12.3456;
+        double temp = value * 100;
+            // Math.round ( ) : 반올림값
+        long temp2 = Math.round(temp);
+        System.out.println("temp2 = " + temp2);
+        double v5 = temp2 / 100.0;
+        System.out.println("v5 = " + v5);
+    }
+}
