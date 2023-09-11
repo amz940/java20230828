3fd987830b1b44c54dce9d0f26916345f8e5826f
Kim Dohyuen <dh_bug@naver.com>
2023-09-11 17:20:31 +0900

    leet code

--- src/ch99reetCode/no1512/Solution.java
+++ src/ch99reetCode/no1512/Solution.java
@@ -1,11 +1,28 @@
 package ch99reetCode.no1512;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
         for ( int num : nums){
-
+            if (map.containsKey(num)){
+                // 처음이 아니면
+                map.put(num , map.get(num)+1);
+            } else {
+                // 처음이면
+                map.put(num, 1);
+            }
         }
 
-        return 0;
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int value = entry.getValue();
+
+            int midSum = (value) * (value-1) / 2;
+            sum += midSum;
+        }
+        return sum;
     }
 }
--- src/ch99reetCode/no1512/SolutionTest.java
+++ src/ch99reetCode/no1512/SolutionTest.java
@@ -8,6 +8,12 @@ class SolutionTest {
 
     @Test
     void numIdenticalPairs() {
-
+        Solution o1 = new Solution();
+        int i = o1.numIdenticalPairs(new int[]{1, 2, 3, 1, 1, 3});
+        assertEquals(4, i);
+        int j = o1.numIdenticalPairs(new int[]{1, 1, 1, 1});
+        assertEquals(6, j);
+        int k = o1.numIdenticalPairs(new int[]{1, 2, 3});
+        assertEquals(0, k);
     }
 }
